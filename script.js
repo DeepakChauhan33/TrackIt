1fe8c5b7b9011a426607d90473a9dffd45d86a06
deepakchauhan33 <deep651965@gmail.com>
2026-04-05 13:55:19 +0530

    Add task in the UI and Filter based on the category

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ const inputTaskBox = document.querySelector('.inputTaskBox');
 const inputTask = document.querySelector('.inputTask');
 const taskAddBtn = document.querySelector('.taskAddBtn');
 const category = document.querySelector('#task-category');
+const taskCant = document.querySelector('.taskCant');
+const filterSection = document.querySelector('#filterSection');
 
 
+function toggleFilterSection() {
+    if (taskArray.length > 0) {
+        filterSection.style.display = "flex";
+    } else {
+        filterSection.style.display = "none";
+    }
+}
 
 
 
@@ -28,6 +37,9 @@ addBtn.addEventListener('click', () => {
 });
 
 
+
+
+// Calling addTask function
 taskAddBtn.addEventListener('click', () => {
     addTask(inputTask, category);
 })
@@ -40,6 +52,8 @@ function addTask(inputTask, category) {
     if (inputTask.value.trim() === "") {
         inputTask.value = "";
         alert("Task field can't br empty");
+        inputTaskBox.style.display = "none";
+
     } else {
 
 
@@ -53,7 +67,8 @@ function addTask(inputTask, category) {
         }
 
         taskArray.push(taskObj);
-        console.log(taskArray);
+        toggleFilterSection();
+        renderTask(taskArray);
         inputTask.value = "";
 
 
@@ -61,3 +76,89 @@ function addTask(inputTask, category) {
 
     }
 }
+
+
+// Function for rendering task in UI
+
+function renderTask(taskArray) {
+    taskCant.innerHTML = "";
+
+    taskArray.forEach(task => {
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+            <div class=" border flex flex-col justify-between   rounded-md p-2 gap-y-2">
+
+          <!-- CheckBox & Task -->
+          <div class="flex justify-between items-start gap-2">
+            <input type="checkbox" class="border h-5 w-5 mt-1.5  ">
+            <p class="w-[100%] max-h-25 overflow-y-auto scroll-smooth border p-2 rounded-sm">
+              ${task.task}
+            </p>
+          </div>
+
+          <!-- Date & Action Button -->
+          <div class="flex justify-between items-end">
+            <span class="text-sm pl-4 font-black text-gray-500/90">${task.date}</span>
+            <div>
+              <button title="edit"
+                class="hover:bg-gray-200 text-gray-500/90 border-0 rounded-md p-1 transition-transform hover:scale-108 duration-100 ease-in-out hover:text-black ">
+                <i class="fa-solid fa-pen  "></i>
+              </button>
+
+              <button title="delete"
+                class="hover:bg-gray-200 text-gray-500/90 border-0 rounded-md p-1 transition-transform hover:scale-108 duration-100 ease-in-out hover:text-red-500/80">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+
+        `
+
+        taskCant.appendChild(li);
+
+        gsap.from(li, {
+            x: 20,
+            opacity: 0,
+            duration: 0.3
+        });
+    })
+}
+
+
+const filterButtons = document.querySelectorAll(".filterBtn");
+
+filterButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        const filterValue = btn.dataset.filter;
+
+        if (filterValue === "all") {
+            renderTask(taskArray);
+        } else {
+            const filteredTasks = taskArray.filter(task => task.category === filterValue);
+            renderTask(filteredTasks);
+        }
+
+    });
+});
+
+
+
+filterButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        // remove active from all
+        filterButtons.forEach(b => b.classList.remove("bg-blue-500", "text-white"));
+
+        // add active to clicked
+        btn.classList.add("bg-blue-500", "text-white");
+
+    });
+});
+
+
+toggleFilterSection();
